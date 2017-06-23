948543d855c3089f7b16be74434d8c04dfceef5d
MarioE27 <marioe27@hotmail.com>
2017-06-23 13:42:22 -0400

    Make utils.lua have spawning-related utility functions instead.

--- CustomQuests/utils.lua
+++ CustomQuests/utils.lua
@@ -1,196 +1,121 @@
--- Counts the number of blocks in the area matching the ID.
-function CountBlocks(x, y, x2, y2, id)
-	local count = 0
-	for i = x, x2 do
-		for j = y, y2 do
-			if MatchesBlock(x, y, id) then
-				count = count + 1
-			end
-		end
-	end
-	return count
+-- Utility function for spawning after any mechanical boss is defeated.
+function AfterAnyMechBoss()
+    return NPC.downedMechBossAny
 end
 
--- Counts the number of blocks in the area matching the ID and frames.
-function CountBlocks(x, y, x2, y2, id, frameX, frameY)
-	local count = 0
-	for i = x, x2 do
-		for j = y, y2 do
-			if MatchesBlock(x, y, id, frameX, frameY) then
-				count = count + 1
-			end
-		end
-	end
-	return count
+-- Utility function for spawning after Golem is defeated.
+function AfterGolem()
+    return NPC.downedGolemBoss
 end
 
--- Counts the number of walls in the area matching the ID.
-function CountWalls(x, y, x2, y2, id)
-	local count = 0
-	for i = x, x2 do
-		for j = y, y2 do
-			if MatchesWall(x, y, id) then
-				count = count + 1
-			end
-		end
-	end
-	return count
+-- Utility function for spawning after the Moon Lord is defeated.
+function AfterMoonLord()
+    return NPC.downedMoonlord
 end
 
--- Finds an NPC by name. (Uses GivenOrTypeName)
-function FindNpcByName(name)
-	for i = 0, 200 do
-		local npc = Main.npc[i]
-		if npc.active and npc.GivenOrTypeName == name then
-			return npc
-		end
-	end
+-- Utility function for spawning after Plantera is defeated.
+function AfterPlantera()
+    return NPC.downedPlantBoss
 end
 
--- Finds an NPC by type.
-function FindNpcByType(type)
-	for i = 0, 200 do
-		local npc = Main.npc[i]
-		if npc.active and npc.netID == type then
-			return npc
-		end
-	end
+-- Utility function for spawning at the cavern level.
+function AtCavernLevel(player)
+    return player.TPlayer.ZoneRockLayerHeight
 end
 
--- Runs a callback for each player in a party.
-function ForEachPlayer(callback)
-	local enumerator = party:GetEnumerator()
-	while enumerator:MoveNext() do
-		callback(enumerator.Current)
-	end
+-- Utility function for spawning at the sky level.
+function AtSkyLevel(player)
+    return player.TPlayer.ZoneSkyHeight
 end
 
--- Determines if the block at the coordinates matches the ID.
-function MatchesBlock(x, y, id)
-	local tile = GetTile(x, y)
-	if id == "air" then
-		return not tile:active() and tile.liquid == 0
-	elseif id == "water" then
-		return not tile:active() and tile.liquid > 0 and tile:liquidType() == 0
-	elseif id == "lava" then
-		return not tile:active() and tile.liquid > 0 and tile:liquidType() == 1
-	elseif id == "honey" then
-		return not tile:active() and tile.liquid > 0 and tile:liquidType() == 2
-	else
-		return tile:active() and GetTileType(tile) == id
-	end
+-- Utility function for spawning at the surface level.
+function AtSurfaceLevel(player)
+    return player.TPlayer.ZoneOverworldHeight
 end
 
--- Determines if the block at the coordinates matches the ID and frames.
-function MatchesBlock(x, y, id, frameX, frameY)
-	local tile = GetTile(x, y)
-	return tile:active() and GetTileType(tile) == id and tile.frameX == frameX and tile.frameY == frameY
+-- Utility function for spawning at the underground level.
+function AtUndergroundLevel(player)
+    return player.TPlayer.ZoneDirtLayerHeight
 end
 
--- Determines if the wall at the coordinates matches the ID.
-function MatchesWall(x, y, id)
-	local tile = GetTile(x, y)
-	return tile.type == id
+-- Utility function for spawning at the underworld level.
+function AtUnderworldLevel(player)
+    return player.TPlayer.ZoneUnderworldHeight
 end
 
--- Adds a chat response trigger.
-function QuickChatResponse(response, onlyLeader, callback)
-	local trigger = ChatResponse(party, response, onlyLeader)
-	trigger.Callback = callback
-	AddTrigger(trigger)
+-- Utility function for spawning during a blood moon.
+function DuringBloodMoon()
+    return Main.bloodMoon
 end
 
--- Adds a condition trigger.
-function QuickCondition(condition, callback)
-	local trigger = Condition(condition)
-	trigger.Callback = callback
-	AddTrigger(trigger)
+-- Utility function for spawning during day.
+function DuringDay()
+    return Main.dayTime
 end
 
--- Adds a drop items trigger.
-function QuickDropItems(name, amount, callback)
-	local trigger = DropItems(party, name, amount)
-	trigger.Callback = callback
-	AddTrigger(trigger)
+-- Utility function for spawning during an eclipse.
+function DuringEclipse()
+    return Main.eclipse
 end
 
--- Adds a gather items trigger.
-function QuickGatherItems(name, amount, callback)
-	local trigger = GatherItems(party, name, amount)
-	trigger.Callback = callback
-	AddTrigger(trigger)
+-- Utility function for spawning during hardmode.
+function DuringHardmode()
+    return Main.hardMode
 end
 
--- Adds an in area trigger.
-function QuickInArea(x, y, x2, y2, isEveryone, callback)
-	local trigger = InArea(party, x, y, x2, y2, isEveryone)
-	trigger.Callback = callback
-	AddTrigger(trigger)
+-- Utility function for spawning during night.
+function DuringNight()
+    return not Main.dayTime
 end
 
--- Adds a kill NPCs trigger.
-function QuickKillNpcs(name, amount, callback)
-	local trigger = KillNpcs(party, name, amount)
-	trigger.Callback = callback
-	AddTrigger(trigger)
+-- Utility function for spawning during rain.
+function DuringRain()
+    return Main.raining
 end
 
--- Adds a wait trigger.
-function QuickWait(seconds, callback)
-	local trigger = Wait(TimeSpan.FromSeconds(seconds))
-	trigger.Callback = callback
-	AddTrigger(trigger)
+-- Utility function for spawning in a corruption biome.
+function InCorruption(player)
+	return player.TPlayer.ZoneCorrupt
 end
 
--- Sets the block at the coordinates to the ID.
-function SetBlock(x, y, id)
-	local tile = GetTile(x, y)
-	if id == "air" then
-		tile:active(false)
-		tile.liquid = 0
-		tile.type = 0
-	elseif id == "water" then
-		tile:active(false)
-		tile.liquid = 255
-		tile:liquidType(0)
-		tile.type = 0
-	elseif id == "lava" then
-		tile:active(false)
-		tile.liquid = 255
-		tile:liquidType(1)
-		tile.type = 0
-	elseif id == "honey" then
-		tile:active(false)
-		tile.liquid = 255
-		tile:liquidType(2)
-		tile.type = 0
-	else
-		tile:active(true)
-		tile.liquid = 0
-		SetTileType(tile, id)
-	end
+-- Utility function for spawning in a crimson biome.
+function InCrimson(player)
+	return player.TPlayer.ZoneCrimson
 end
 
--- Sets the blocks in the area.
-function SetBlocks(x, y, x2, y2, id)
-	for i = x, x2 do
-		for j = y, y2 do
-			SetBlock(i, j, id)
-		end
-	end
+-- Utility function for spawning in a desert biome.
+function InDesert(player)
+	return player.TPlayer.ZoneDesert
 end
 
--- Sets the walls at the coordinates.
-function SetWall(x, y, id)
-	local tile = GetTile(x, y)
-	tile.wall = id
+-- Utility function for spawning in the dungeon.
+function InDungeon(player)
+	return player.TPlayer.ZoneDungeon
 end
 
--- Sets the walls in the area.
-function SetWalls(x, y, x2, y2, id)
-	for i = x, x2 do
-		for j = y, y2 do
-			SetWall(i, j, id)
-		end
-	end
+-- Utility function for spawning in a forest biome (not a special biome).
+function InForest(player)
+    return not player.TPlayer.ZoneCorrupt and not player.TPlayer.ZoneCrimson and not player.TPlayer.ZoneDesert
+       and not player.TPlayer.ZoneDungeon and not player.TPlayer.ZoneHoly    and not player.TPlayer.ZoneSnow
+       and not player.TPlayer.ZoneJungle  and not player.TPlayer.ZoneMeteor
+end
+
+-- Utility function for spawning in a hallow biome.
+function InHallow(player)
+	return player.TPlayer.ZoneHoly
+end
+
+-- Utility function for spawning in an ice biome.
+function InIce(player)
+	return player.TPlayer.ZoneSnow
+end
+
+-- Utility function for spawning in a jungle biome.
+function InJungle(player)
+	return player.TPlayer.ZoneJungle
+end
+
+-- Utility function for spawning in a meteor.
+function InMeteor(player)
+	return player.TPlayer.ZoneMeteor
 end
