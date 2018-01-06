238ebf5b0d3fb6cc87738c5d87cdc867c044d4fa
Timothy Barela <tbarela@timbarela.com>
2018-01-05 16:15:01 -0800

    Added some doc strings to LuaHelperExtensions.

--- CustomNpcs/LuaHelperExtensions.cs
+++ CustomNpcs/LuaHelperExtensions.cs
@@ -11,6 +11,12 @@ namespace CustomNpcs
 {
 	public static class LuaHelperExtensions
 	{
+		/// <summary>
+		/// Helper to get a single, typed result from a LuaFunction or SafeLuaFunction call.
+		/// </summary>
+		/// <typeparam name="T">Type of expected result.</typeparam>
+		/// <param name="luaResults">Object array of results</param>
+		/// <returns>The result value if successful, or null otherwise.</returns>
 		public static T? GetResult<T>(this object[] luaResults) where T : struct
 		{
 			if(luaResults!=null && luaResults.Length > 0 && luaResults[0] is T)
@@ -19,6 +25,12 @@ namespace CustomNpcs
 			return null;
 		}
 
+		/// <summary>
+		/// Creates a SafeLuaFunction wrapper for the named function. 
+		/// </summary>
+		/// <param name="lua">Lua instance.</param>
+		/// <param name="functionName">Function name.</param>
+		/// <returns>A SafeLuaFunction.</returns>
 		public static SafeLuaFunction GetSafeFunction(this Lua lua, string functionName)
 		{
 			var luaFunction = lua[functionName] as LuaFunction;
