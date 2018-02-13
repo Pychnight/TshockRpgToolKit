f8130c5cb83383c2f6f921a5b38ed75998078211
Timothy Barela <tbarela@timbarela.com>
2018-02-12 19:26:24 -0800

    Add project files.

---
+++ Banking/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    public class Class1
+    {
+    }
+}
