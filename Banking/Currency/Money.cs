10a16351d3cc14bf82bc22ff9c32217287dc7da2
Timothy Barela <tbarela@timbarela.com>
2018-03-12 10:16:27 -0700

    Add stub for potential Money type.
    Disabled.

---
+++ Banking/Currency/Money.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Currency
+{
+	public struct Money
+	{
+		internal decimal Value;
+		internal short CurrencyIndex;//index of the currency
+		internal short CurrencyManagerVersion;//each time the currency manager loads the currencies from the config, its version is incremented.
+												//Money tracks this so that we do not reference/index into a new currency.
+	}
+}
