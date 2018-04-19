4de38ccec95fcff98188727cc956f599222d96c6
Timothy Barela <tbarela@timbarela.com>
2018-04-19 02:13:05 -0700

    Reorder QuestInfo properties, add changelog.txt to csproj.

--- CustomQuests/Quests/QuestInfo.cs
+++ CustomQuests/Quests/QuestInfo.cs
@@ -1,20 +1,27 @@
-﻿namespace CustomQuests.Quests
+﻿using System;
+
+namespace CustomQuests.Quests
 {
     /// <summary>
     ///     Represents information about a quest.
     /// </summary>
-    public sealed class QuestInfo
+	public sealed class QuestInfo
     {
-        /// <summary>
-        ///     Gets or sets the description.
-        /// </summary>
-        public string Description { get; set; }
+		/// <summary>
+		///     Gets or sets the name.
+		/// </summary>
+		public string Name { get; set; }
 
-        /// <summary>
-        ///     Gets or sets the friendly name.
-        /// </summary>
-        public string FriendlyName { get; set; }
+		/// <summary>
+		///     Gets or sets the friendly name.
+		/// </summary>
+		public string FriendlyName { get; set; }
 
+		/// <summary>
+		///     Gets or sets the description.
+		/// </summary>
+		public string Description { get; set; }
+        
         /// <summary>
         ///     Gets or sets the script path.
         /// </summary>
@@ -30,20 +37,15 @@
         /// </summary>
         public int MaxPartySize { get; set; }
 
-        /// <summary>
-        ///     Gets or sets the maximum number of repeats.
-        /// </summary>
-        public int MaxRepeats { get; set; }
+		/// <summary>
+		///     Gets or sets the minimum party size.
+		/// </summary>
+		public int MinPartySize { get; set; }
 
-        /// <summary>
-        ///     Gets or sets the minimum party size.
-        /// </summary>
-        public int MinPartySize { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the name.
-        /// </summary>
-        public string Name { get; set; }
+		/// <summary>
+		///     Gets or sets the maximum number of repeats.
+		/// </summary>
+		public int MaxRepeats { get; set; }
 
         /// <summary>
         ///     Gets or sets the required region name.
