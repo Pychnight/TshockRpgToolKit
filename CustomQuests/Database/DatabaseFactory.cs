4cff53aecf156cea35e58cef36f12f6e4e9b509f
Timothy Barela <tbarela@timbarela.com>
2018-05-23 08:31:21 -0700

    Add some stubs for new db handling.

---
+++ CustomQuests/Database/DatabaseFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQuests.Database
+{
+	public class DatabaseFactory
+	{
+		public static IDatabase LoadOrCreateDatabase(string databaseType, string connectionString)
+		{
+			IDatabase db = null;
+
+			switch( databaseType )
+			{
+				//case "redis":
+				//	db = new RedisDatabase(connectionString);
+				//	break;
+
+				//case "mysql":
+				//	db = new MySqlDatabase(connectionString);
+				//	break;
+
+				//case "sqlite":
+				//default:
+				//	db = new SqliteDatabase(connectionString);
+				//	break;
+			}
+
+			return db;
+		}
+	}
+}
---
+++ CustomQuests/Database/IDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQuests.Database
+{
+	public interface IDatabase
+	{
+		string ConnectionString { get; set; }
+
+		//IEnumerable<BankAccount> Load();
+		//void Save(IEnumerable<BankAccount> accounts);
+
+		//void Create(BankAccount account);
+		//void Create(IEnumerable<BankAccount> accounts);
+
+		//void Update(BankAccount account);
+		//void Update(IEnumerable<BankAccount> accounts);
+
+		//void Delete(BankAccount account);
+		//void Delete(IEnumerable<BankAccount> accounts);
+	}
+}
