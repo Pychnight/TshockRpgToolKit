34771069e5df32a6be62f1b18553c5a6ec367993
Timothy Barela <tbarela@timbarela.com>
2018-06-15 17:37:26 -0700

    Put invasion props under "Basic Properties" category.

--- Models/Invasion.cs
+++ Models/Invasion.cs
@@ -27,6 +27,7 @@ namespace CustomNpcsEdit.Models
 		/// <summary>
 		///     Gets the name.
 		/// </summary>
+		[Category("Basic Properties")]
 		[JsonProperty(Order = 0)]
 		public string Name
 		{
@@ -41,37 +42,43 @@ namespace CustomNpcsEdit.Models
 		/// <summary>
 		///     Gets the script path.
 		/// </summary>
-		[JsonProperty(Order = 1)]
+		[Category("Basic Properties")]
 		[Editor(typeof(FileNameEditor), typeof(System.Drawing.Design.UITypeEditor))]
+		[JsonProperty(Order = 1)]
 		public string ScriptPath { get; set; }
 
 		/// <summary>
 		///     Gets the NPC point values.
 		/// </summary>
+		[Category("Basic Properties")]
 		[JsonProperty(Order = 2)]
 		public Dictionary<string, int> NpcPointValues { get; set; } = new Dictionary<string, int>();
 
 		/// <summary>
 		///     Gets the completed message.
 		/// </summary>
+		[Category("Basic Properties")]
 		[JsonProperty(Order = 3)]
 		public string CompletedMessage { get; set; } = "The invasion has ended!";
 
 		/// <summary>
 		///     Gets a value indicating whether the invasion should occur at spawn only.
 		/// </summary>
+		[Category("Basic Properties")]
 		[JsonProperty(Order = 4)]
 		public bool AtSpawnOnly { get; set; }
 
 		/// <summary>
 		///     Gets a value indicating whether the invasion should scale by the number of players.
 		/// </summary>
+		[Category("Basic Properties")]
 		[JsonProperty(Order = 5)]
 		public bool ScaleByPlayers { get; set; }
 
 		/// <summary>
 		///     Gets the waves.
 		/// </summary>
+		[Category("Basic Properties")]
 		[JsonProperty(Order = 6)]
 		public List<WaveDefinition> Waves { get; set; } = new List<WaveDefinition>();
 
