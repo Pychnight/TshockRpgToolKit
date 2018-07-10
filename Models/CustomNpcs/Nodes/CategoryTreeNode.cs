26c99832ea7e86a232719bcd47dd14af69a415ae
Timothy Barela <tbarela@timbarela.com>
2018-07-09 23:44:50 -0700

    Fix - CategoryTreeNodes should accept CategoryTreeNodes as Siblings.

--- Models/CustomNpcs/Nodes/CategoryTreeNode.cs
+++ Models/CustomNpcs/Nodes/CategoryTreeNode.cs
@@ -93,7 +93,7 @@ namespace RpgToolsEditor.Models.CustomNpcs
 		public override bool CanAcceptDraggedNode(ModelTreeNode node)
 		{
 			return node is NpcTreeNode || //make sibling
-					( node is CategoryTreeNode<Npc, NpcTreeNode> && Parent == null ) || //make sibling
+					( node is CategoryTreeNode<TModel, TNode> && Parent == null ) || //make sibling
 					( node is IncludeTreeNode<TModel, TNode>); //make child
 		}
 
