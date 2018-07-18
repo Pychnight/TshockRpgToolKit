4e0dd0eaf00391799d2ab4deff96d9dce2640893
Timothy Barela <tbarela@timbarela.com>
2018-07-18 07:28:11 -0700

    Clean up.
    Re-ordered ShopDefinition members for serialization.

--- NpcShops/Shops/NpcShopDefinition.cs
+++ NpcShops/Shops/NpcShopDefinition.cs
@@ -10,12 +10,19 @@ namespace NpcShops.Shops
     /// <summary>
     ///     Represents an NPC shop definition.
     /// </summary>
-    public sealed class NpcShopDefinition
+	[JsonObject(MemberSerialization.OptIn)]
+    public class NpcShopDefinition
     {
-        /// <summary>
-        ///     Gets the closing time.
-        /// </summary>
-        [JsonProperty(Order = 2)]
+		/// <summary>
+		///     Gets the opening time.
+		/// </summary>
+		[JsonProperty(Order = 1)]
+		public string OpeningTime { get; private set; }
+
+		/// <summary>
+		///     Gets the closing time.
+		/// </summary>
+		[JsonProperty(Order = 2)]
         public string ClosingTime { get; private set; }
 
         /// <summary>
@@ -27,51 +34,45 @@ namespace NpcShops.Shops
 		/// <summary>
 		///     Gets the closed message.
 		/// </summary>
-		[JsonProperty(Order = 9)]
+		[JsonProperty(Order = 4)]
 		public string ClosedMessage { get; private set; }
 
 		/// <summary>
-		///     Gets the opening time.
+		///     Gets the restock time.
 		/// </summary>
-		[JsonProperty(Order = 1)]
-        public string OpeningTime { get; private set; }
+		[JsonProperty(Order = 5)]
+		public TimeSpan RestockTime { get; private set; } = TimeSpan.FromMinutes(1);
 
-        /// <summary>
-        ///     Gets the region name.
-        /// </summary>
-        [JsonProperty(Order = 0)]
-        public string RegionName { get; private set; }
+		/// <summary>
+		///     Gets the sales tax rate.
+		/// </summary>
+		[JsonProperty(Order = 6)]
+		public double SalesTaxRate { get; private set; } = 0.07;
+
+		/// <summary>
+		///     Gets the region name.
+		/// </summary>
+		[JsonProperty(Order = 7)]
+		public string RegionName { get; private set; }
 
 		/// <summary>
 		///		Gets the town npc types that this shop overrides.
 		/// </summary>
 		[JsonProperty(Order = 8)]
 		public List<int> OverrideNpcTypes { get; private set; } = new List<int>();
+		
+		/// <summary>
+		///     Gets the list of shop items.
+		/// </summary>
+		[JsonProperty(Order = 9)]
+		public IList<ShopItemDefinition> ShopItems { get; private set; } = new List<ShopItemDefinition>();
 
-        /// <summary>
-        ///     Gets the restock time.
-        /// </summary>
-        [JsonProperty(Order = 6)]
-        public TimeSpan RestockTime { get; private set; } = TimeSpan.FromMinutes(1);
-
-        /// <summary>
-        ///     Gets the sales tax rate.
-        /// </summary>
-        [JsonProperty(Order = 7)]
-        public double SalesTaxRate { get; private set; } = 0.07;
-
-        /// <summary>
-        ///     Gets the list of shop items.
-        /// </summary>
-        [JsonProperty(Order = 5)]
-        public IList<ShopCommandDefinition> ShopCommands { get; private set; } = new List<ShopCommandDefinition>();
-
-        /// <summary>
-        ///     Gets the list of shop items.
-        /// </summary>
-        [JsonProperty(Order = 4)]
-        public IList<ShopItemDefinition> ShopItems { get; private set; } = new List<ShopItemDefinition>();
-
+		/// <summary>
+		///     Gets the list of shop items.
+		/// </summary>
+		[JsonProperty(Order = 10)]
+		public IList<ShopCommandDefinition> ShopCommands { get; private set; } = new List<ShopCommandDefinition>();
+				
 		/// <summary>
 		/// 
 		/// </summary>
