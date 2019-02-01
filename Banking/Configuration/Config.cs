024e00c66be3d20f3c429bd89afbc4f489bb338b
Timothy Barela <tbarela@timbarela.com>
2019-01-31 16:51:07 -0800

    Use new Validation, and shared DatabaseConfig.

--- Banking/Configuration/Config.cs
+++ Banking/Configuration/Config.cs
@@ -16,12 +16,25 @@ namespace Banking.Configuration
 		public static Config Instance { get; internal set; }
 		
 		[JsonProperty(Order = 0)]
-		public DatabaseConfig Database { get; set; } = new DatabaseConfig();
+		public DatabaseConfig Database { get; set; } = new DatabaseConfig("sqlite", $"uri=file://banking/db.sqlite,Version=3");
 
 		[JsonProperty(Order = 1)]
 		public string ScriptPath { get; set; }
 				
 		[JsonProperty(Order = 2)]
 		public VotingConfig Voting { get; set; } = new VotingConfig();
+
+		public override ValidationResult Validate()
+		{
+			var result = new ValidationResult();
+
+			if (Database == null)
+				result.Errors.Add(new ValidationError($"Database config is null."));
+
+			if (Voting == null)
+				result.Warnings.Add(new ValidationWarning($"Voting config is null."));
+
+			return result;
+		}
 	}
 }
