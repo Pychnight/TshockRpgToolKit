5bc855bb477e5fa746586a4d14b2c6f3803860ba
Timothy Barela <tbarela@timbarela.com>
2019-05-02 04:27:44 -0700

    CustomSkills: Stub in more skill definition data
    Added initial skill level defs.

--- CustomSkills/CustomSkill.cs
+++ CustomSkills/CustomSkill.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
@@ -12,13 +13,41 @@ namespace CustomSkills
 	[JsonObject(MemberSerialization.OptIn)]
 	public class CustomSkillDefinition
 	{
-		//public string CustomID { get; set; }
-		//unique identifier.
+		/// <summary>
+		/// Gets or sets a unique identifier to reference this skill type.
+		/// </summary>
 		[JsonProperty(Order = 0)]
 		public string Name { get; set; } = "NewCustomSkill";
+		//public string CustomID { get; set; }
 
-		//user readable summary of skill.
+		/// <summary>
+		/// Gets or sets a user readable summary of this skill.
+		/// </summary>
 		[JsonProperty(Order = 1)]
 		public string Description { get; set; } = "No description.";
+
+		/// <summary>
+		/// Gets or sets a comma separated list of permissions required to learn this skill.
+		/// </summary>
+		[JsonProperty(Order = 2)]
+		public string PermissionsToLearn { get; set; } = "";
+
+		/// <summary>
+		/// Gets or sets a comma separated list of permissions required to use this skill.
+		/// </summary>
+		[JsonProperty(Order = 3)]
+		public string PermissionsToUse { get; set; } = "";
+		
+		/// <summary>
+		/// Gets or sets whether to notify the user that this skill is ready to use again.
+		/// </summary>
+		[JsonProperty(Order = 4)]
+		public bool NotifyUserOnCooldown { get; set; } = false;
+
+		/// <summary>
+		/// Gets or sets the list of CustomSkillLevelDefinitions for this skill.
+		/// </summary>
+		[JsonProperty(Order = 5)]
+		public List<CustomSkillLevelDefinition> Levels { get; set; } = new List<CustomSkillLevelDefinition>();
 	}
 }
---
+++ CustomSkills/CustomSkillLevelDefinition.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+
+namespace CustomSkills
+{
+	[JsonObject(MemberSerialization.OptIn)]
+	public class CustomSkillLevelDefinition
+	{
+		//public int Level { get; set; }
+
+		/// <summary>
+		/// Gets or sets a one time cost to cast this level of spell.
+		/// </summary>
+		[JsonProperty(Order = 0)]
+		public string CastingCost { get; set; }
+
+		/// <summary>
+		/// Gets or sets an interval of time that must pass between repeat usages for this level of spell.
+		/// </summary>
+		[JsonProperty(Order = 1)]
+		public int CastingCooldown { get; set; }
+
+		/// <summary>
+		/// Gets or sets a cost deducted while charging this level of spell.
+		/// </summary>
+		[JsonProperty(Order = 2)]
+		public string ChargingCost { get; set; }
+
+		/// <summary>
+		/// Gets or sets the duration of time required to charge this level of spell.
+		/// </summary>
+		[JsonProperty(Order = 3)]
+		public int ChargingDuration { get; set; }
+
+		/// <summary>
+		/// Gets or sets the interval of time between charging cost deductions for this level of spell.
+		/// </summary>
+		[JsonProperty(Order = 4)]
+		public int ChargingCostInterval { get; set; }
+
+		/// <summary>
+		/// Gets or sets whether this level of spell can be interrupted.
+		/// </summary>
+		[JsonProperty(Order = 5)]
+		public bool CanInterrupt { get; set; } = false;
+
+		/// <summary>
+		/// Gets or sets whether the caster is allowed to move while this level of spell is charging or firing.
+		/// </summary>
+		[JsonProperty(Order = 6)]
+		public bool CanCasterMove { get; set; } = false;
+
+		/// <summary>
+		/// Gets or sets the number of uses before this spell levels up.
+		/// </summary>
+		[JsonProperty(Order = 7)]
+		public int UsesToLevelUp { get; set; } = 0;
+	}
+}
